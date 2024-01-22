c553bc661e1daeaa6d707f392e2512f827770bc3
Georg Schwab <georg.schwab@xentara.io>
2024-01-22 10:16:03 +0100

    Added display name to element classes

--- src/TemplateInstance.hpp
+++ src/TemplateInstance.hpp
@@ -34,7 +34,10 @@ public:
 	/// @brief The class object containing meta-information about this element type
 	/// @todo change class name
 	/// @todo assign a unique UUID
-	using Class = ConcreteClass<"TemplateInstance", "deadbeef-dead-beef-dead-beefdeadbeef"_uuid>;
+	/// @todo change display name
+	using Class = ConcreteClass<"TemplateInstance",
+		"deadbeef-dead-beef-dead-beefdeadbeef"_uuid,
+		"template microservice instance">;
 
 	/// @name Virtual Overrides for skill::Element
 	/// @{
