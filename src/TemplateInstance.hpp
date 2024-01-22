f8992a152d2455b46306dd63a0689aaa556ce246
Georg Schwab <georg.schwab@xentara.io>
2024-01-22 16:41:58 +0100

    Added element category callback to skill elements

--- src/TemplateInstance.hpp
+++ src/TemplateInstance.hpp
@@ -7,6 +7,7 @@
 #include "Output.hpp"
 
 #include <xentara/memory/Array.hpp>
+#include <xentara/model/ElementCategory.hpp>
 #include <xentara/process/Event.hpp>
 #include <xentara/process/Task.hpp>
 #include <xentara/skill/Element.hpp>
@@ -50,9 +51,11 @@ public:
 
 	auto makeReadHandle(const model::Attribute &attribute) const noexcept -> std::optional<data::ReadHandle> final;
 
-	auto realize() -> void final;
-
-	auto prepare() -> void final;
+	auto category() const noexcept -> model::ElementCategory final
+	{
+		/// @todo Return an appropriate category
+		return model::ElementCategory::Microservice;
+	}
 	
 	/// @}
 
@@ -119,6 +122,10 @@ private:
 	/// @{
 
 	auto load(utils::json::decoder::Object &jsonObject, config::Context &context) -> void final;
+
+	auto realize() -> void final;
+
+	auto prepare() -> void final;
 
 	/// @}
 
